7e40a118f45af0f6dab64a0b3b7134da4cb4f7a1
Rafaela Silva <rafa_ss04@hotmail.com>
2020-11-28 13:08:42 -0300

    Inicio do cadastro de usuarios.

---
+++ NewDevGuide/NewDevGuide.DTO/DTO/EnderecoDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewDevGuide.DTO.DTO
+{
+    public class EnderecoDto
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public int? Numero { get; set; }
+        public bool SemNumero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public int CodCidade { get; set; }
+        public string Estado { get; set; }
+        public string Complemento { get; set; }
+
+
+
+    }
+}
---
+++ NewDevGuide/NewDevGuide.DTO/DTO/UsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewDevGuide.DTO.DTO
+{
+    public class UsuarioDto
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public EnderecoDto Endereco { get; set; }
+        public EnderecoDto EnderecoCobranca { get; set; }
+    }
+}
