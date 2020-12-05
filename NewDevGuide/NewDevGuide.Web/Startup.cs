a7c0c9d5c01cbbdebff2a2d8947ee38b70794fa3
Rafaela Silva <rafa_ss04@hotmail.com>
2020-12-05 13:10:55 -0300

    Adição dos Collections

--- NewDevGuide/NewDevGuide.Web/Startup.cs
+++ NewDevGuide/NewDevGuide.Web/Startup.cs
@@ -36,11 +36,11 @@ namespace NewDevGuide.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
